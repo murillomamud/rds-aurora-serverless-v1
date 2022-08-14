4d003f60ab02cd195e7931dba90bd30c6826384d
Murillo Mamud <murillomamud@gmail.com>
2022-08-14 00:49:44 -0300

    first commit

---
+++ terraform/rds.tf
@@ -0,0 +1,26 @@
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-postgresql"  
+  engine_mode             = "serverless"  
+  database_name           = var.db_name  
+  # enable_http_endpoint    = true  
+  master_username         = var.db_username
+  master_password         = var.db_password
+  backup_retention_period = 7
+  apply_immediately = true
+  skip_final_snapshot     = true
+
+  scaling_configuration {
+  auto_pause               = true
+  min_capacity             = 2    
+  max_capacity             = 384
+  seconds_until_auto_pause = 300
+  timeout_action           = "ForceApplyCapacityChange"
+  }  
+
+  tags = {
+    Name = "My aurora demo cluster"
+    component = "database"
+  }
+
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,12 @@
+variable "db_name" {
+  default = "myauroradb"
+}
+
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_username"{
+  default = "postgres"
+}
